a04b600fba26cb1d438b7f8bb36eb0d3c729d711
elvis67 <elvissouzaalves0345@gmail.com>
2026-07-08 12:26:33 -0300

    Refactor mouse logic for drawing state management

--- src/projeto_paint/controlador/app_controller.py
+++ src/projeto_paint/controlador/app_controller.py
@@ -32,54 +32,18 @@ class ControladorPaint:
     def atualizar_cor_preenchimento(self, cor):
         self.cor_preenchimento = cor
 
-    # LÓGICA DO MOUSE 
   
+    # NOVA LÓGICA DO MOUSE 
+    
     def inicia_desenho(self, event):
-        self.inicio_x = event.x
-        self.inicio_y = event.y
-        self.figura_atual = None
-
-        if self.ferramenta_atual == "Polígono":
-            self.pontos_poligono.extend([event.x, event.y])
-            self.visao_interface.canvas.delete("temporario")
-            
-            if len(self.pontos_poligono) >= 6:
-                self.figura_atual = Poligono(self.pontos_poligono, self.cor_borda, self.cor_preenchimento)
-                self.figura_atual.desenhar(self.visao_interface.canvas, tags="temporario")
-        else:
-            self.coordenadas_atuais = [event.x, event.y]
+        self.estado_atual.inicia(event)
 
     def atualiza_desenho(self, event):
-        if self.ferramenta_atual == "Polígono":
-            return
-
-        self.visao_interface.canvas.delete("temporario")
-        ferramenta = self.ferramenta_atual
-
-        if ferramenta == "Mão livre":
-            self.coordenadas_atuais.extend([event.x, event.y])
-            self.figura_atual = MaoLivre(self.coordenadas_atuais, self.cor_borda)
-        else:
-            ClasseFigura = self.classes_figuras[ferramenta]
-            self.figura_atual = ClasseFigura(
-                self.inicio_x, self.inicio_y, event.x, event.y, 
-                self.cor_borda, self.cor_preenchimento
-            )
-
-        if self.figura_atual:
-            self.figura_atual.desenhar(self.visao_interface.canvas, tags="temporario")
+        self.estado_atual.atualiza(event)
 
     def finaliza_desenho(self, event):
-        if self.ferramenta_atual != "Polígono":
-            self.visao_interface.canvas.dtag("temporario", "figura_definitiva")
-            if self.figura_atual:
-                self.modelo_desenho.adicionar_figuras(self.figura_atual)
-            self.figura_atual = None
+        self.estado_atual.finaliza(event)
 
     def encerra_poligono(self, event=None):
-        if self.pontos_poligono:
-            self.visao_interface.canvas.dtag("temporario", "figura_definitiva")
-            if self.figura_atual:
-                self.modelo_desenho.adicionar_figuras(self.figura_atual)
-            self.pontos_poligono = []
-            self.figura_atual = None
+        if hasattr(self.estado_atual, 'encerra'):
+            self.estado_atual.encerra()
